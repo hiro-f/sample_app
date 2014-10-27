2a7017126acc8a4a8d0381eb796f8f796d9826bc
hiro <1310370168b@kindai.ac.jp>
2014-10-27 13:57:41 +0900

    Finish user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141016061754) do
+ActiveRecord::Schema.define(version: 20141027042017) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20141016061754) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,11 @@
 FactoryGirl.define do
-  factory :user do
-    name     "Hiroki Fujita"
-    email    "hoge@example.com"
-    password "testtest"
-    password_confirmation "testtest"
-  end
+    factory :user do
+        sequence(:name)  { |n| "Person #{n}" }
+        sequence(:email) { |n| "Person_#{n}@example.com" }
+        password "foobar"
+        password_confirmation "foobar"
+        factory :admin do
+            admin true
+        end
+    end
 end
